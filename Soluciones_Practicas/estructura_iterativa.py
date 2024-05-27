bfea53bd0d789e3b46393af7a7585f8b416b8d9e
HermanAguilar <aguilar.hfd@gmail.com>
2024-05-26 22:54:57 -0300

    commit1

--- Soluciones_Practicas/estructura_iterativa.py
+++ Soluciones_Practicas/estructura_iterativa.py
@@ -1,4 +1,10 @@
-num= int(input("ingrese un numero: "))
+# num= int(input("ingrese un numero: "))
 
-for i in range (num):
-    print (i)
+# for i in range (num):
+#     print (i)
+
+
+# numero= int(input("indica el numero de la tabla: "))
+
+# for i in range (1,11):
+#     print (numero, " x ",i, "=", numero*i)
